1a3c3e9601ea8e8a27a61f4771e9c4dfc7fee661
asad <asadfrombronx2yahoo.com>
2014-01-11 22:59:24 -0500

    modify

--- BitPageObjectProjrct-01Jan14/src/test/java/com/bit/bluefly/test/BaseTest.java
+++ BitPageObjectProjrct-01Jan14/src/test/java/com/bit/bluefly/test/BaseTest.java
@@ -5,25 +5,18 @@ import org.junit.Before;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest 
-
 {
-
 	public FirefoxDriver driver;
-	
 	@Before
 	public void start()
 	{
 		driver = new FirefoxDriver();
 	}
-	
 	@After
-	public void end() throws Exception
+	public void end() throws Throwable
 	{
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		driver.quit();
 	}
-	
-	
-	
-	
+
 }
